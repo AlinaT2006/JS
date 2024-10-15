e0f527a464589a43fb0ce43bcbf5be42b407b270
Alina <alinatuhtaeva@gmail.com>
2024-10-15 22:06:08 +0500

    first homework

---
+++ a/script.js
@@ -0,0 +1,33 @@
+
+var name = prompt ('Введите своё имя')
+console.log('Меня зовут ' + name);
+var age = prompt ('Введите свой возраст')
+console.log('Мне ' + age + ' лет');
+var ready = confirm ('Are you ready to start??')
+
+var ans = prompt ('6 + 3 = ?') 
+var num ='Пример 1: 6 + 3 = ' + (6 + 3) 
+console.log(num + ' Ваш ответ = ' + ans);
+
+var ans = prompt ('6 - 3 = ?') 
+var num ='Пример 2: 6 - 3 = ' + (6 - 3) 
+console.log(num + ' Ваш ответ = ' + ans);
+
+var ans = prompt ('6 / 3 = ?') 
+var num ='Пример 3: 6 / 3 = ' + (6 / 3) 
+console.log(num + ' Ваш ответ = ' + ans)
+
+var ans = prompt ('6 * 3 = ?') 
+var num ='Пример 4: 6 * 3 = ' + (6 * 3) 
+console.log(num + ' Ваш ответ = ' + ans);
+
+var ans = prompt ('10 % 3 = ?') 
+var num ='Пример 5: 10 % 3 = ' + (10 % 3) 
+console.log(num + ' Ваш ответ = ' + ans);
+
+alert ('Готовы узнать правильные ответы?')
+alert ('Are you sure?')
+alert ('Перейдите в консоль')
+console.log('Отличная работа!');
+
+
